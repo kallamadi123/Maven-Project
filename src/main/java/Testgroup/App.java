413f7afee1631856d5d12ea7a07f04aa3f1dbd60
Praveen <praveen.kumar00791@gmail.com>
2022-01-25 16:28:28 +0530

    Build File

---
+++ src/main/java/Testgroup/App.java
@@ -0,0 +1,13 @@
+package Testgroup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
